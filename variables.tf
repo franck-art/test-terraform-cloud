a7e99bcecd7b41502273ea5c994fb3571d9a24a9
franck-art <58075364+franck-art@users.noreply.github.com>
2020-11-19 21:58:26 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "name_instance" {
+type = "string"
+description = "name of instance"
+}
+
+variable "type_machine" {
+type = "string"
+description = "type of machine"
+
+}
+
+variable "zone" {
+type = "string"
+description = "zone of instance"
+
+}
