9448c0c2ec8f35dc6c98e6bc7333a95dc62b1b45
franck-art <58075364+franck-art@users.noreply.github.com>
2020-11-19 22:14:49 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("YOUR_PATH_TO_JSON_FILE/xxx.json")
+  #credentials = file("YOUR_PATH_TO_JSON_FILE/xxx.json")
   project     = "YOUR PROJECT ID"
   region      = "us-central1"
 }
@@ -7,7 +7,7 @@ provider "google" {
 
 terraform {
   backend "gcs" {
-    credentials = "YOUR_PATH_TO_JSON_FILE/xxx.json"
+  #  credentials = "YOUR_PATH_TO_JSON_FILE/xxx.json"
     bucket  = "franck-sceau"
     prefix  = "terraform/terraform_state"
   }
